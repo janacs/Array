b744c8fd1604512bceb1f525e9f72ab3091d37ca
Janderson <jandersonsouza_phn@hotmail.com>
2024-09-18 12:42:17 -0300

    primeiroCommit

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Array");
+
+// Array - Conjunto de dados
+// Variável especial que armazena mais de um dado
+// Variável que guarda apenas um dado, ex: let nome = "Luan";
+
+// Como criamos um array
+// Usamos os COLCHETES [], cada item tem que ser separado por vírgula
+
+let animais = ["Gato", "Cachorro", "Arara", "Camelo", "Macaco", "Coruja"];
+
+console.log(animais[3]);
